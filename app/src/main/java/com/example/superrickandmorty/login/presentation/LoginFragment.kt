2ce224822f3664af22fb53150f654077b7fa49bf
unknown <oscartografo@gmail.com>
2020-08-21 20:22:58 -0400

    Implementamos ViewBinding en Login

---
+++ app/src/main/java/com/example/superrickandmorty/login/presentation/LoginFragment.kt
@@ -0,0 +1,20 @@
+package com.example.superrickandmorty.login.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.superrickandmorty.R
+import com.example.superrickandmorty.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLoginBinding.bind(view)
+    }
+
+}
