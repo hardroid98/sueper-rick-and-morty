2a9511b791cc56f10476657ee5787d7c7b652c79
José Morales <jos.moralesb@gmail.com>
2020-08-22 10:01:49 -0400

    se agrego loginVM y LoginVMF

--- app/src/main/java/com/example/superrickandmorty/login/presentation/LoginFragment.kt
+++ app/src/main/java/com/example/superrickandmorty/login/presentation/LoginFragment.kt
@@ -2,10 +2,10 @@ package com.example.superrickandmorty.login.presentation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.superrickandmorty.R
 import com.example.superrickandmorty.databinding.FragmentLoginBinding
 import com.example.superrickandmorty.login.data.local.LocalLoginRepository
@@ -21,18 +21,60 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     private lateinit var binding: FragmentLoginBinding
     private lateinit var loginUseCase: LoginUseCase
     private lateinit var repository: LoginRepository
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var loginViewModelFactory: LoginViewModelFactory
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupDependencies()
         binding = FragmentLoginBinding.bind(view)
+        setupLiveData()
         setupListener()
+    }
+
+    private fun setupLiveData() {
+        loginViewModel.getLiveData().observe(
+            viewLifecycleOwner,
+            Observer {state ->
+                handleState(state)
+            }
+        )
+    }
+
+    private fun handleState(state: LoginState?) {
+        when(state){
+            is LoginState.LoadingState -> mostrarCargando()
+            is LoginState.ContrasenaIncorrecta -> mostrarErrorContrasena()
+            is LoginState.Complete -> mostrarLoginCorrecto(state.result)
+            is LoginState.Error -> mostrarError()
+        }
+    }
+
+    private fun mostrarError() {
+        Toast.makeText(requireContext(),"Ups, algo ha pasado", Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun mostrarLoginCorrecto(result: Usuario?) {
+        Toast.makeText(requireContext(),"Bienvenido ${result?.name}", Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun mostrarErrorContrasena() {
+        Toast.makeText(requireContext(),"Contraseña incorrecta", Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun mostrarCargando() {
+        Toast.makeText(requireContext(),"Cargando", Toast.LENGTH_SHORT).show()
     }
 
     private fun setupDependencies() {
         repository = LocalLoginRepository()
         loginUseCase = LoginUseCase(repository)
+        loginViewModelFactory = LoginViewModelFactory(loginUseCase)
+        loginViewModel = ViewModelProvider(this, loginViewModelFactory).get(LoginViewModel::class.java)
+
     }
 
     private fun setupListener() {
@@ -40,31 +82,13 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             btnIngresar.setOnClickListener {
                 val usuario = etUsuario.text.toString()
                 val contrasena = etContrasena.text.toString()
-                callUseCase(usuario, contrasena)
+                callLoginViewModel(usuario, contrasena)
             }
         }
     }
 
-    private fun callUseCase(usuario: String, contrasena: String) {
-        compositeDisposable.add(
-            loginUseCase.execute(usuario, contrasena)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    {result -> handleResult(result)},
-                    {error -> handleError(error)}
-                )
-        )
+    private fun callLoginViewModel(usuario: String, contrasena: String) {
+        loginViewModel.doLogin(usuario, contrasena)
     }
-
-    private fun handleResult(result: Usuario) {
-
-    }
-
-    private fun handleError(error: Throwable) {
-        Toast.makeText(requireContext(), "Error: {${error.message}", Toast.LENGTH_LONG).show()
-    }
-
-
 
 }
